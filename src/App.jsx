4c76d9889d1ff561fa623125cf2c220e45d7a2bb
Sumaiya Afrin <sumaiya.afrin1008@gmail.com>
2025-03-25 15:00:33 +0600

    fixed scroll-to-top

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,14 @@ import Skills from "./components/Skills";
 import Contact from "./components/Contact";
 import Education from "./components/Education";
 import Projects from "./components/Projects";
-import ScrollToTop from "react-scroll-to-top";
-import { CiSaveUp1 } from "react-icons/ci";
+// import ScrollToTop from "react-scroll-to-top";
+// import { CiSaveUp1 } from "react-icons/ci";
 
 function App() {
   return (
     <>
       <Ball />
-      <ScrollToTop
+      {/* <ScrollToTop
         smooth
         style={{
           backgroundColor: "#071e1f",
@@ -35,7 +35,7 @@ function App() {
             }}
           />
         }
-      />
+      /> */}
 
       <Header />
       <Banner />
